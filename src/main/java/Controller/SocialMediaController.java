044e330a62c7984e620f24a9cba5aa6531cc94a0
Joey Pirich <joeypirich@hotmail.com>
2025-03-30 20:25:31 +0000

    Created AccountDAO.java and AccountService.java. Implemented account registration feature across DAO, service, and controller layers.

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -1,7 +1,14 @@
 package Controller;
 
+import Model.Account;
+import Model.Message;
+import Service.AccountService;
+// import Service.MessageService;
+
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller. The endpoints you will need can be
@@ -9,6 +16,12 @@ import io.javalin.http.Context;
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
 public class SocialMediaController {
+    private AccountService accountService;
+    // private MessageService messageService;
+
+    public SocialMediaController() {
+        accountService = new AccountService();
+    }
     /**
      * In order for the test cases to work, you will need to write the endpoints in the startAPI() method, as the test
      * suite must receive a Javalin object from this method.
@@ -17,6 +30,8 @@ public class SocialMediaController {
     public Javalin startAPI() {
         Javalin app = Javalin.create();
         app.get("example-endpoint", this::exampleHandler);
+
+        app.post("register", this::registerHandler);
 
         return app;
     }
@@ -29,5 +44,20 @@ public class SocialMediaController {
         context.json("sample text");
     }
 
-
+    /**
+     * This is an example handler for an example endpoint.
+     * @param context The Javalin Context object manages information about both the HTTP request and response.
+     */
+    private void registerHandler(Context ctx) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        Account account = mapper.readValue(ctx.body(), Account.class);
+        // System.out.println("account: " + account);
+        Account addedAccount = accountService.registerAccount(account);
+        // System.out.println("addedAccount: " + addedAccount);
+        if (addedAccount != null) {
+            ctx.json(mapper.writeValueAsString(addedAccount));
+        } else {
+            ctx.status(400);
+        }
+    }
 }
---
+++ src/main/java/DAO/AccountDAO.java
@@ -0,0 +1,64 @@
+package DAO;
+
+import Util.ConnectionUtil;
+import Model.Account;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class AccountDAO {
+    /**
+     * Insert an account into the account table
+     * @param account
+     * @return the given account with generated account ID if successful, else null
+     */
+    public Account insertAccount(Account account) {
+        Connection connection = ConnectionUtil.getConnection();
+        
+        try {
+            String sql = "INSERT INTO account (username, password) VALUES (?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            preparedStatement.setString(1, account.getUsername());
+            preparedStatement.setString(2, account.getPassword());
+
+            preparedStatement.executeUpdate();
+
+            ResultSet pkeyResultSet = preparedStatement.getGeneratedKeys();
+            if (pkeyResultSet.next()) {
+                // TODO: maybe should be `int generatedAccountId = (int) pkeyResultSet.getLong(1);`?
+                int generatedAccountId = pkeyResultSet.getInt(1);
+                return new Account(generatedAccountId, account.getUsername(), account.getPassword());
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    /**
+     * Return whether a given username is available and unclaimed for an
+     * account to be registered under it. Defaults to false in case of
+     * exception.
+     * 
+     * @param username
+     * @return true if an account exists with the given username
+     */
+    public boolean usernameAvailable(String username) {
+        Connection connection = ConnectionUtil.getConnection();
+        String checkSql = "SELECT * FROM account WHERE username=?";
+
+        try {
+            PreparedStatement checkPreparedStatement = connection.prepareStatement(checkSql);
+            checkPreparedStatement.setString(1, username);
+            ResultSet checkResultSet = checkPreparedStatement.executeQuery();
+            return !checkResultSet.next();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+}
